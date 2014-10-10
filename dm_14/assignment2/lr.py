38643abe0d9f57bdb1b5fbb3da21c48ba39e7751
yuzt <yuzt@nlp.nju.edu.cn>
2014-10-10 15:50:27 +0800

    update ass2

--- dm_14/assignment2/lr.py
+++ dm_14/assignment2/lr.py
@@ -3,25 +3,28 @@
 import numpy as np
 import pickle
 from collections import Counter
-from sklearn.cross_validation import StratifiedKFold
 
 class SGDLR(object):
-    def fit(self,X,y):
-        self.rate = 10
+    def fit(self,X,y,iter,learning_rate):
+        self.rate = learning_rate
         n_sample,n_feature = X.shape
         theta = np.ones(n_feature+1)
         X = np.column_stack((np.ones(n_sample),X))
-        for i in range(n_sample):
-            theta = theta - self.rate*(1.0/(1.0+np.power(np.e,-np.dot(theta.T,X[i])))-y[i])*X[i]
+        for i in range(iter):
+            for i in range(n_sample):
+                h = 1/(1.0+np.power(np.e,-np.dot(theta,X[i].T)))
+                theta = theta - self.rate*(h-y[i])*X[i]
         self.theta = theta
 
     def predict(self,X):
         n_sample,n_feature = X.shape
         X = np.column_stack((np.ones(n_sample),X))
-        return 1.0/(1.0+np.power(np.e,-np.dot(X,self.theta.T)))
+        h = 1/(1.0+np.power(np.e,-np.dot(X,self.theta.T)))
+        return h
         
+
 class MultiClassifier(object):
-    def fit(self,X,y):
+    def fit(self,X,y,iter,learning_rate):
         labels_counter = Counter(y)
         labels = labels_counter.keys()
         n_label = len(labels)
@@ -33,7 +36,7 @@ class MultiClassifier(object):
             for i,value in enumerate(y):
                 if value == label:
                     s_y[i] = 1.0
-            sgdlr.fit(X,s_y)
+            sgdlr.fit(X,s_y,iter,learning_rate)
             self.sgdlr_classifiers.append(sgdlr)
         self.labels = labels 
 
@@ -52,28 +55,32 @@ class MultiClassifier(object):
             predict_labels.append(self.labels[index])
         return predict_labels
 
-def main():
-    with open('lily.pickle') as f:
-        lily = pickle.load(f)
 
-    labels = lily['labels']
-    tf = lily['tf']
-    tf = tf.todense()
-    tf = np.array(tf)
-    tfidf = lily['tfidf']
-    tfidf = tfidf.todense()
-    tfidf = np.array(tfidf)
+def kfoldcross(a,fold):
+    n_sample = len(a)
+    piece = n_sample/fold
+    train_index = list()
+    test_index = list()
+    for i in range(fold):
+        all = range(n_sample)
+        if i < fold-1:
+            test = range(i*piece,(i+1)*piece)
+        else:
+            test = range(i*piece,n_sample)
+        train = list(set(all)-set(test))
+        train_index.append(train)
+        test_index.append(test)
+    return zip(train_index,test_index)
 
-    skf = StratifiedKFold(labels,10,shuffle=True)
-
-    classifier = MultiClassifier()
-    features = tf
+def test_classifier(classifier,features,labels,iter=5,learning_rate=0.1):
+    kfc = kfoldcross(labels,10)
     accuracies = list()
-    for train_index,test_index in skf:
-        X = tf[train_index]
+    for train_index,test_index in kfc:
+        X = features[train_index]
         y = labels[train_index]
-        classifier.fit(X,y)
+        classifier.fit(X,y,iter,learning_rate)
         predict_labels = classifier.predict(features[test_index])
+
         reuslt_labels = labels[test_index]
         count = 0
         for i in range(len(predict_labels)):
@@ -85,6 +92,29 @@ def main():
     print 'mean:',np.mean(accuracies)
     print 'var:',np.var(accuracies)
     print 'std',np.std(accuracies)
+
+def regularization(X):
+    max_values = np.max(X,axis=0)
+    min_values = np.min(X,axis=0)
+    X = (X-min_values) / (max_values-min_values)
+    return X
+
+def main():
+    with open('lily.pickle') as f:
+        lily = pickle.load(f)
+
+    labels = lily['labels']
+    tf = lily['tf']
+    tf = tf.todense()
+    tf = np.array(tf)
+    tfidf = lily['tfidf']
+    tfidf = tfidf.todense()
+    tfidf = np.array(tfidf)
+    #tfidf = regularization(tfidf)
+
+    mulclassifier = MultiClassifier()
+    test_classifier(mulclassifier,tf,labels,iter=5,learning_rate=0.1)
+    test_classifier(mulclassifier,tfidf,labels,iter=5,learning_rate=5.0)
 
 if __name__ == "__main__":
     main()
